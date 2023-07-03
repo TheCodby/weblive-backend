bb596b4920cfdc4b8feee16ab1d12390b46c48d4
Codby <87335804+TheCodby@users.noreply.github.com>
2023-07-03 05:04:59 +0300

    change db provider to postgres

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 var cors = require('cors');
 var corsOptions = {
   origin: '*',
 };
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const appOptions = {
+    cors: true,
+    httpsOptions: process.env.SSL_KEY_PATH
+      ? {
+          key: fs.readFileSync(process.env.SSL_KEY_PATH),
+          cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+        }
+      : {},
+  };
+  const app = await NestFactory.create(AppModule, appOptions);
   app.use(cors(corsOptions));
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(process.env.PORT || 443);
 }
 bootstrap();
