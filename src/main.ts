dc622537b40f024e841b036546c265009c6f3925
Codby <87335804+TheCodby@users.noreply.github.com>
2023-07-04 01:56:16 +0300

    setting up deployment action

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
 var cors = require('cors');
 var corsOptions = {
   origin: '*',
 };
 
 async function bootstrap() {
-  const appOptions = {
-    cors: true,
-    httpsOptions: process.env.SSL_KEY_PATH
-      ? {
-          key: fs.readFileSync(process.env.SSL_KEY_PATH),
-          cert: fs.readFileSync(process.env.SSL_CERT_PATH),
-        }
-      : {},
-  };
-  const app = await NestFactory.create(AppModule, appOptions);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(cors(corsOptions));
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  await app.listen(3000);
 }
 bootstrap();
