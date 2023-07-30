1303d60e8284981b88b1dece5e228a621197fa08
Codby <87335804+TheCodby@users.noreply.github.com>
2023-07-31 02:00:44 +0300

    added follow schema

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,31 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Param, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from '../guards/auth.guard';
+import { RequestWithUser } from '../interfaces/user';
 
 @Controller('users')
 @UseGuards(AuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   @Get(':username')
-  async getUserByUsername(@Param('username') username: string) {
-    return await this.usersService.getUserByUsername(username);
+  async getUserByUsername(
+    @Param('username') username: string,
+    @Req() req: RequestWithUser,
+  ) {
+    return await this.usersService.getUserByUsername(req.user.id, username);
+  }
+  @Post(':userId/follow')
+  async followUser(
+    @Param('userId') userId: number,
+    @Req() req: RequestWithUser,
+  ) {
+    return await this.usersService.followUser(req.user.id, +userId);
+  }
+  @Post(':userId/unfollow')
+  async unfollowUser(
+    @Param('userId') userId: number,
+    @Req() req: RequestWithUser,
+  ) {
+    return await this.usersService.unfollowUser(req.user.id, +userId);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,7 +4,7 @@ import { PrismaService } from '../database/prisma.service';
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
-  async getUserByUsername(username: string) {
+  async getUserByUsername(requesterId: number, username: string) {
     const profile = await this.prisma.user.findUnique({
       where: {
         username: username,
@@ -30,7 +30,46 @@ export class UsersService {
         },
       },
     });
+    const isFollowing = await this.prisma.follow.findUnique({
+      where: {
+        followerId_followingId: {
+          followerId: requesterId,
+          followingId: profile.id,
+        },
+      },
+    });
     if (!profile) throw new NotFoundException('User not found');
-    return profile;
+    return {
+      ...profile,
+      isFollowing: !!isFollowing,
+    };
+  }
+  async followUser(followerId: number, userId: number) {
+    if (followerId === userId)
+      throw new NotFoundException('You cannot follow yourself');
+    await this.prisma.follow.create({
+      data: {
+        followerId: followerId,
+        followingId: userId,
+      },
+    });
+    return {
+      message: `You are now following this user`,
+    };
+  }
+  async unfollowUser(followerId: number, userId: number) {
+    if (followerId === userId)
+      throw new NotFoundException('You cannot unfollow yourself');
+    await this.prisma.follow.delete({
+      where: {
+        followerId_followingId: {
+          followerId: followerId,
+          followingId: userId,
+        },
+      },
+    });
+    return {
+      message: `You are no longer following this user`,
+    };
   }
 }
